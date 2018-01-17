d79191f848ee6ff943dfa62684557f9fd6535e03
yangyh <136585913@qq.com>
2018-01-17 11:57:23 +0800

    跟新

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,49 +1,59 @@
 import React from 'react';
-import {StyleSheet, View, Text, Button, Image} from 'react-native';
-
-import {StackNavigator, TabNavigator} from 'react-navigation';
-
+import { StyleSheet, View, Text, Button, Image } from 'react-native';
+import { StackNavigator, TabNavigator } from 'react-navigation';
 import ChatScreen from './ChatScreen';
 import MinePage from './MinePage';
 
 class HomePage extends React.Component {
 
-  static navigationOptions = {
-    headerTitle: '首页', //设置标题内容
-    headerBackTitle: null,
-    headerStyle: {
-      height: 45,
-      backgroundColor: '#8AD65F'
-    },
-    headerTitleStyle: {
-      color: '#fff',
-      fontSize: 16,
-      alignSelf: 'center'
-    },
-    headerRight: <Image
-      source={require('./images/home_navicon_search.png')}
-      style={{
-      width: 24,
-      height: 24,
-      marginRight: 16
-    }}/>,
-    headerLeft: <Image
-        source={require('./images/home_navicon_saoma.png')}
-        style={{
-        width: 24,
-        height: 24,
-        marginLeft: 16
-      }}/>
-  }
+	static navigationOptions = {
+		headerTitle: '首页', //设置标题内容
+		headerBackTitle: null,
+		headerStyle: {
+			height: 45,
+			backgroundColor: '#8AD65F'
+		},
+		headerTitleStyle: {
+			color: '#fff',
+			fontSize: 16,
+			alignSelf: 'center'
+		},
+		headerRight: < Image
+		source = {
+			require('./images/home_navicon_search.png')
+		}
+		style = {
+			{
+				width: 24,
+				height: 24,
+				marginRight: 16
+			}
+		}
+		/>,
+		headerLeft: < Image
+		source = {
+			require('./images/home_navicon_saoma.png')
+		}
+		style = {
+			{
+				width: 24,
+				height: 24,
+				marginLeft: 16
+			}
+		}
+		/>
+	}
 
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props);
+	}
 
-  render() {
-    const {navigate} = this.props.navigation;
-    return (
-      <View>
+	render() {
+		const {
+			navigate
+		} = this.props.navigation;
+		return(
+			<View>
         <View style={{flex: 5, flexDirection: 'row'}}>
           <View style={{flex: 1, height: 50, backgroundColor: '#8AD65F' ,alignItems: 'center'}}>
             <Image
@@ -63,19 +73,21 @@ class HomePage extends React.Component {
         {/* <View>
           <Button onPress={() => navigate('Chat', {user: 'Sybil'})} title="点击跳转"/> 
         </View> */}
-      </View> 
-    )
-  }
+      </View>
+		)
+	}
 }
 /**
  * 底部导航组件
  */
 const MainScreenNavigator = TabNavigator({
-  Home: {
-    screen: HomePage,
-    navigationOptions: {
-      tabBarLabel: '首页',
-      tabBarIcon: ({tintColor}) => (<Image
+	Home: {
+		screen: HomePage,
+		navigationOptions: {
+			tabBarLabel: '首页',
+			tabBarIcon: ({
+				tintColor
+			}) => (<Image
         source={require('./images/home_tabicon_home.png')}
         style={[
         {
@@ -83,13 +95,15 @@ const MainScreenNavigator = TabNavigator({
         },
         styles.icon
       ]}/>)
-    }
-  },
-  OrdorList: {
-    screen: MinePage,
-    navigationOptions: {
-      tabBarLabel: '订单',
-      tabBarIcon: ({tintColor}) => (<Image
+		}
+	},
+	OrdorList: {
+		screen: MinePage,
+		navigationOptions: {
+			tabBarLabel: '订单',
+			tabBarIcon: ({
+				tintColor
+			}) => (<Image
         source={require('./images/home_tabicon_dingd.png')}
         style={[
         {
@@ -97,13 +111,15 @@ const MainScreenNavigator = TabNavigator({
         },
         styles.icon
       ]}/>)
-    }
-  },
-  MsgList: {
-    screen: MinePage,
-    navigationOptions: {
-      tabBarLabel: '消息',
-      tabBarIcon: ({tintColor}) => (<Image
+		}
+	},
+	MsgList: {
+		screen: MinePage,
+		navigationOptions: {
+			tabBarLabel: '消息',
+			tabBarIcon: ({
+				tintColor
+			}) => (<Image
         source={require('./images/home_tabicon_message.png')}
         style={[
         {
@@ -111,13 +127,15 @@ const MainScreenNavigator = TabNavigator({
         },
         styles.icon
       ]}/>)
-    }
-  },
-  Certificate: {
-    screen: MinePage,
-    navigationOptions: {
-      tabBarLabel: '我的',
-      tabBarIcon: ({tintColor}) => (<Image
+		}
+	},
+	Certificate: {
+		screen: MinePage,
+		navigationOptions: {
+			tabBarLabel: '我的',
+			tabBarIcon: ({
+				tintColor
+			}) => (<Image
         source={require('./images/home_tabicon_mine.png')}
         style={[
         {
@@ -125,47 +143,47 @@ const MainScreenNavigator = TabNavigator({
         },
         styles.icon
       ]}/>)
-    }
-  }
+		}
+	}
 }, {
-  animationEnabled: false, // 切换页面时不显示动画
-  tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
-  swipeEnabled: false, // 禁止左右滑动
-  backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
-  tabBarOptions: {
-    activeTintColor: '#8AD65F', // 文字和图片选中颜色
-    inactiveTintColor: '#999', // 文字和图片默认颜色
-    showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
-    indicatorStyle: {
-      height: 0
-    }, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了
-    style: {
-      backgroundColor: '#EDEDED', // TabBar 背景色
-      height: 55
-    },
-    labelStyle: {
-      fontSize: 12, // 文字大小
-      marginTop: 0
-    }
-  }
+	animationEnabled: false, // 切换页面时不显示动画
+	tabBarPosition: 'bottom', // 显示在底端，android 默认是显示在页面顶端的
+	swipeEnabled: false, // 禁止左右滑动
+	backBehavior: 'none', // 按 back 键是否跳转到第一个 Tab， none 为不跳转
+	tabBarOptions: {
+		activeTintColor: '#8AD65F', // 文字和图片选中颜色
+		inactiveTintColor: '#999', // 文字和图片默认颜色
+		showIcon: true, // android 默认不显示 icon, 需要设置为 true 才会显示
+		indicatorStyle: {
+			height: 0
+		}, // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了
+		style: {
+			backgroundColor: '#EDEDED', // TabBar 背景色
+			height: 55
+		},
+		labelStyle: {
+			fontSize: 12, // 文字大小
+			marginTop: 0
+		}
+	}
 });
 
 const styles = StyleSheet.create({
-  icon: {
-    height: 22,
-    width: 22,
-    resizeMode: 'contain',
-    marginTop: 0
-  }
+	icon: {
+		height: 22,
+		width: 22,
+		resizeMode: 'contain',
+		marginTop: 0
+	}
 });
 
 const SimpleApp = StackNavigator({
-  Home: {
-    screen: MainScreenNavigator
-  },
-  Chat: {
-    screen: ChatScreen
-  }
+	Home: {
+		screen: MainScreenNavigator
+	},
+	Chat: {
+		screen: ChatScreen
+	}
 });
 
 export default SimpleApp;
